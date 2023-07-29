8e22f7e086435a051b0f71164ba4af2bc2a7f523
Rodrigo <liberti1991@gmail.com>
2023-07-29 11:32:43 -0300

    ajustes no app

--- src/screens/newTask/schema.ts
+++ src/screens/newTask/schema.ts
@@ -3,13 +3,13 @@ import { msgSchema } from "../../message/messageSchema";
 
 export const schemaNewTask = object({
   task: string()
-    .transform((value) => value.trimStart().replace(/\s/g, ""))
+    .transform((value) => value.trimStart())
     .min(4, msgSchema.min(4))
     .max(30, msgSchema.max(30))
     .required(msgSchema.required),
 
   description: string()
-    .transform((value) => value.trimStart().replace(/\s/g, ""))
+    .transform((value) => value.trimStart())
     .min(10, msgSchema.min(10))
     .max(150, msgSchema.max(150))
     .required(msgSchema.required),
